d96ab99355b18e87fed374a558519845730adb82
joq62 <joq62@c50>
2024-11-12 21:06:06 +0100

    corr

--- apps/appl/src/test_appl.erl
+++ apps/appl/src/test_appl.erl
@@ -33,96 +33,11 @@ start()->
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
    
     ok=test_1(),
-    ok=test_2(),
-    ok=test_3(),
-    ok=test_4(),
-    ok=test_5(),
-    ok=test_6(),
+    
     ok.
 
 
-%%--------------------------------------------------------------------
-%% @doc
-%% 
-%% @end
-%%--------------------------------------------------------------------
 
-test_6()->
-    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
-    {ok,Host}=net:gethostname(),
-    N=list_to_atom("appl@"++Host),
-    X=rpc:call(N,appl,test_diff_result,[],6000),
-    {error,["M:F [A]) with reason",lib_appl,test_diff_result,
-	    [add_test,add_test,add,[20,22],5000],
-	    "Reason=",["Different results",42,[glurk]]]
-    }=X,
-    ok.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% 
-%% @end
-%%--------------------------------------------------------------------
-
-test_5()->
-    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
-    {ok,Host}=net:gethostname(),
-    N=list_to_atom("appl@"++Host),
-    X=rpc:call(N,appl,test_badrpc_no_nodes,[],6000),
-    {error,["M:F [A]) with reason",lib_appl,test_badrpc_no_nodes,
-	    [add_test,erlang,glurk,[],5000],
-	    "Reason=",
-	    ["NonExistingNodes and BadRpcList ",
-	     [glurk@c50],
-	     [{badrpc,{'EXIT',{undef,[{erlang,glurk,[],[]}]}}}]]]}=X,
-    ok.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% 
-%% @end
-%%--------------------------------------------------------------------
-
-test_4()->
-    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
-    {ok,Host}=net:gethostname(),
-    N=list_to_atom("appl@"++Host),
-    {error,[ErrorMap]}=rpc:call(N,appl,test_crash,[],6000),
-    error=maps:get(event,ErrorMap),
-    appl=maps:get(module,ErrorMap),
-    handle_call=maps:get(function,ErrorMap),
-    %maps:get(line,ErrorMap),
-    []=maps:get(args,ErrorMap),
-    badarith=maps:get(reason,ErrorMap),
-    %maps:get(stacktrace,ErrorMap),
-    ok.
-%%--------------------------------------------------------------------
-%% @doc
-%% 
-%% @end
-%%--------------------------------------------------------------------
-
-test_3()->
-    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
-    {ok,Host}=net:gethostname(),
-    N=list_to_atom("appl@"++Host),
-    {error,["M:F [A]) with reason",lib_appl,call,
-	    [glurk,add_test,add,[20,22],5000],
-	    "Reason=",["No target resources ",glurk]]
-    }=rpc:call(N,appl,call,[glurk,add_test,add,[20,22],5000],6000),
-    ok.
-%%--------------------------------------------------------------------
-%% @doc
-%% 
-%% @end
-%%--------------------------------------------------------------------
-
-test_2()->
-    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
-    {ok,Host}=net:gethostname(),
-    N=list_to_atom("appl@"++Host),
-    {ok,42}=rpc:call(N,appl,call,[add_test,add_test,add,[20,22],5000],6000),
-    ok.
 %%--------------------------------------------------------------------
 %% @doc
 %% 
@@ -132,8 +47,8 @@ test_2()->
 test_1()->
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
     {ok,Host}=net:gethostname(),
-    ApplNode=list_to_atom("appl@"++Host),
-    pong=rpc:call(ApplNode,appl,ping,[],5000),
+    ApplNode=list_to_atom(?NodeName++"@"++Host),
+    pong=rpc:call(ApplNode,?Appl,ping,[],5000),
     {Year,Month,Day}=date(),
     {ok,Year,Month,Day}=rpc:call(ApplNode,appl,template_call,[glurk],5000),
     ok.
